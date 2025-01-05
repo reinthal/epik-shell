6e6a604d6223717e623b55da3656568f0791a8fe
Edwin <edwinnnzx@gmail.com>
2025-01-06 04:08:05 +0800

    update screenshot popover

--- src/widgets/quicksettings/buttons/ScreenshotQS.jsx
+++ src/widgets/quicksettings/buttons/ScreenshotQS.jsx
@@ -1,31 +1,58 @@
-import { App } from "astal/gtk4";
+import { App, Gtk } from "astal/gtk4";
 import ScreenRecord from "../../../utils/screen-record";
 import { QSMenuButton } from "../QSButton";
 import { WINDOW_NAME } from "../QSWindow";
+import { Gio } from "astal";
+import { timeout } from "astal";
 
 export default function ScreenshotQS() {
   const screenRecord = ScreenRecord.get_default();
+  const actions = [
+    {
+      name: "Full",
+      callback: () => {
+        App.toggle_window(WINDOW_NAME);
+        timeout(200, () => {
+          screenRecord.screenshot(true);
+        });
+      },
+    },
+    {
+      name: "Partial",
+      callback: () => {
+        App.toggle_window(WINDOW_NAME);
+        timeout(200, () => {
+          screenRecord.screenshot();
+        });
+      },
+    },
+  ];
+
+  const menu = new Gio.Menu();
+  actions.forEach(({ name }) => {
+    menu.append(name, `ss.${name}`);
+  });
+
+  const Popover = new Gtk.PopoverMenu();
+  Popover.set_menu_model(menu);
 
   return (
-    <QSMenuButton label={"Screenshot"} iconName={"gnome-screenshot-symbolic"}>
-      <popover has_arrow={false}>
-        <box vertical>
-          <button
-            onClicked={() => {
-              App.toggle_window(WINDOW_NAME);
-              screenRecord.screenshot(true);
-            }}
-            label={"Full"}
-          />
-          <button
-            onClicked={() => {
-              App.toggle_window(WINDOW_NAME);
-              screenRecord.screenshot();
-            }}
-            label={"Partial"}
-          />
-        </box>
-      </popover>
+    <QSMenuButton
+      setup={(self) => {
+        const actionGroup = new Gio.SimpleActionGroup();
+
+        actions.forEach((actionInfo) => {
+          const action = new Gio.SimpleAction({ name: actionInfo.name });
+          action.connect("activate", actionInfo.callback);
+          actionGroup.add_action(action);
+        });
+
+        self.insert_action_group("ss", actionGroup);
+      }}
+      label={"Screenshot"}
+      iconName={"gnome-screenshot-symbolic"}
+    >
+      {Popover}
     </QSMenuButton>
   );
 }
